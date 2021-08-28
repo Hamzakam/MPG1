1ca03f3bf1c46f8c406c75c8841bbc99896ef759
HamzaKam <mail.hamzakam@gmail.com>
2021-08-28 18:48:49 +0530

    changes to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,55 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-const Posts = (props)=>{
+const UpDownCounter = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const onLike = () => setLikeCount(likeCount + 1);
+  const onDislike = () => setLikeCount(likeCount - 1);
   return (
-    <div className="Post">
-      <h3>{props.title}</h3>
+    <div className="UpDownCounter">
+      <button id="like" onClick={onLike}>
+        ▲
+      </button>
+      <p>{likeCount}</p>
+      <button id="dislike" onClick={onDislike}>
+        ▼
+      </button>
+    </div>
+  );
+};
+const Post = (props) => {
+  return (
+    <div className="Post" id={props.id}>
+      <UpDownCounter />
+      <h3 className="PostTitle">{props.title}</h3>
       <p>{props.textContent}</p>
     </div>
-  )
-}
+  );
+};
 
-const App = ()=> {
+const PostFeed = () => {
+  let posts = [
+    {key:1, title: "Who am i", textContent: "it's me mario" },
+    {key:2, title: "Half as Creative", textContent: "I am an uncreative fuck" },
+  ];
+  
+  return (
+    <div className="Posts">
+      {
+        posts.map(post=>(<Post key={post.key} title={post.title} textContent={post.textContent}/>))
+      }
+    </div>
+  );
+};
+
+const App = () => {
+
   return (
     <div className="App">
       <h1>PeerHub</h1>
-      <Posts title="Who am i" textContent="it's me mario"/>
-      <Posts title="Half as Creative" textContent="I am an uncreative fuck"/>
+      <PostFeed />
     </div>
   );
-}
+};
 
 export default App;
