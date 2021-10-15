169656c170b4c4bcea3008539c1aee2ecffecab6
Hamzakam <mail.hamzakam@gmail.com>
2021-10-15 15:44:19 +0530

    Changed file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,23 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Login from "./Components/Login/Login";
 import Navbar from "./Components/Navbar";
-import PostFeed from "./Components/Post";
-import { BrowserRouter as Router , Switch , Route} from "react-router-dom";
+import PostFeed from "./Components/Post/Post";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Pages";
 
 const App = () => {
-    return (
-    
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-            <h1>PeerHub</h1>
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <h1>PeerHub</h1>
 
-            <PostFeed />
-            <Login />
-           
-        </Router>
-
-    );
+      <PostFeed />
+      <Login />
+    </Router>
+  );
 };
 
 export default App;
